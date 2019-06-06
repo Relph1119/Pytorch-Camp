4637ed8a953f4e1abb80c6004d25ddf7dceed139
Hurf <huruifeng1202@163.com>
2019-06-06 10:37:53 +0800

    Initial commit

---
+++ src/Week1/lesson1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: HuRuiFeng
+@file: lesson1.py
+@time: 2019/6/6 10:07
+@desc: 验证安装的环境
+"""
+import torch
+
+print(torch.__version__)
+print(torch.cuda.is_available())
+print('hello, world.')
